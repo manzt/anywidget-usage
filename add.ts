27f3eaf9e9fa39c08be8dca60baf1d019df54487
Trevor Manz <trevor.j.manz@gmail.com>
2024-03-18 01:16:54 -0400

    add stars script

--- add.ts
+++ add.ts
@@ -18,6 +18,7 @@ export async function fetch_repo_info(
 	let url = new URL(repo, base);
 	let response = await fetch(url, { headers });
 	let json = await response.json();
+
 	return {
 		repo: json.full_name,
 		description: json.description,
@@ -35,72 +36,74 @@ function maybe_exit<T>(x: T | symbol): T {
 	return x;
 }
 
-p.intro("Add a new widget project");
+if (import.meta.main) {
+	p.intro("Add a new widget project");
 
-let repo = await p.text({ message: "Repository" });
-maybe_exit(repo);
-let uses_anywidget = await p.confirm({ message: "Use anywidget?" });
-maybe_exit(uses_anywidget);
-let hidive = await p.confirm({ message: "Hidive?", initialValue: false });
-maybe_exit(hidive);
-let s = p.spinner();
-s.start("Fetching repository info");
-let info = await fetch_repo_info(repo as string);
-s.stop();
-let description = await p.text({
-	message: "Description",
-	initialValue: info.description,
-});
-maybe_exit(description);
-let widget_created = await p.text({
-	message: "Widget Created",
-	initialValue: "null",
-	validate(value) {
-		if (value === "null") return;
-		if (!/^\d{4}-\d{2}-\d{2}$/.test(value)) {
-			return "Must be a date in the format YYYY-MM-DD";
-		}
-	},
-});
-maybe_exit(widget_created);
-let kind = await p.select({
-	message: "Integration kind",
-	options: [
-		{ value: "widget", label: "Widget" },
-		{ value: "framework", label: "Framework" },
-	],
-});
-maybe_exit(kind);
+	let repo = await p.text({ message: "Repository" });
+	maybe_exit(repo);
+	let uses_anywidget = await p.confirm({ message: "Use anywidget?" });
+	maybe_exit(uses_anywidget);
+	let hidive = await p.confirm({ message: "Hidive?", initialValue: false });
+	maybe_exit(hidive);
+	let s = p.spinner();
+	s.start("Fetching repository info");
+	let info = await fetch_repo_info(repo as string);
+	s.stop();
+	let description = await p.text({
+		message: "Description",
+		initialValue: info.description,
+	});
+	maybe_exit(description);
+	let widget_created = await p.text({
+		message: "Widget Created",
+		initialValue: "null",
+		validate(value) {
+			if (value === "null") return;
+			if (!/^\d{4}-\d{2}-\d{2}$/.test(value)) {
+				return "Must be a date in the format YYYY-MM-DD";
+			}
+		},
+	});
+	maybe_exit(widget_created);
+	let kind = await p.select({
+		message: "Integration kind",
+		options: [
+			{ value: "widget", label: "Widget" },
+			{ value: "framework", label: "Framework" },
+		],
+	});
+	maybe_exit(kind);
 
-let exclude_file = uses_anywidget
-	? "assets/exclude_repos_anywidget.txt"
-	: "assets/exclude_repos_ipywidgets.txt";
-let add_to_ignore = await p.confirm({
-	message: `Add repo to exclude file: ${exclude_file}?`,
-});
-maybe_exit(add_to_ignore);
+	let exclude_file = uses_anywidget
+		? "assets/exclude_repos_anywidget.txt"
+		: "assets/exclude_repos_ipywidgets.txt";
+	let add_to_ignore = await p.confirm({
+		message: `Add repo to exclude file: ${exclude_file}?`,
+	});
+	maybe_exit(add_to_ignore);
 
-let entry = {
-	repo: info.repo,
-	url: `https://github.com/${info.repo}`,
-	hidive: hidive,
-	widget_created: widget_created === "null" ? null : widget_created,
-	description: description,
-	use_anywidget: uses_anywidget,
-	repo_created: info.repo_created,
-	kind: kind,
-};
+	let entry = {
+		repo: info.repo,
+		url: `https://github.com/${info.repo}`,
+		hidive: hidive,
+		widget_created: widget_created === "null" ? null : widget_created,
+		description: description,
+		use_anywidget: uses_anywidget,
+		repo_created: info.repo_created,
+		kind: kind,
+	};
 
-{
-	let url = new URL("assets/repos.json", import.meta.url);
-	let data = await Deno.readTextFile(url).then(JSON.parse);
-	data.push(entry);
-	await Deno.writeTextFile(url, JSON.stringify(data, null, "\t") + "\n");
-}
+	{
+		let url = new URL("assets/repos.json", import.meta.url);
+		let data = await Deno.readTextFile(url).then(JSON.parse);
+		data.push(entry);
+		await Deno.writeTextFile(url, JSON.stringify(data, null, "\t") + "\n");
+	}
 
-{
-	let url = new URL(exclude_file, import.meta.url);
-	let data = Deno.readTextFileSync(url).split("\n").filter(Boolean);
-	data.push(info.repo);
-	Deno.writeTextFileSync(url, data.join("\n") + "\n");
+	{
+		let url = new URL(exclude_file, import.meta.url);
+		let data = Deno.readTextFileSync(url).split("\n").filter(Boolean);
+		data.push(info.repo);
+		Deno.writeTextFileSync(url, data.join("\n") + "\n");
+	}
 }
---
+++ stars.ts
@@ -0,0 +1,15 @@
+import { fetch_repo_info } from "./add.ts";
+
+let wait = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+let url = new URL("assets/repos.json", import.meta.url);
+let partial_repos = await Deno.readTextFile(url).then(JSON.parse);
+
+let results = [];
+for (let item of partial_repos) {
+	let info = await fetch_repo_info(item.repo);
+	results.push({ ...info, ...item });
+	await wait(1000);
+}
+
+console.log(results);
